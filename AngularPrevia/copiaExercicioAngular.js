9a5f48b067a0f1ed2d3eb1b357001ec4dac7086b
KAUA HENRIQUE ALMEIDA SALVADOR <20251148060019@ifrn.local>
2025-10-01 16:15:58 -0300

    Adcionando Estrutura de repetição

---
+++ AngularPrevia/copiaExercicioAngular.js
@@ -0,0 +1,31 @@
+// import { Component } from '@angular/core';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// @Component({
+//   selector: 'app-root',
+//   imports: [FormsModule],
+//   template: `
+//     <h2>Lista de tarefas</h2>
+//     <hr />
+//     <p>
+//       Tarefa: <input [(ngModel)]="tarefa" /> 
+//       <button>Adicionar</button>
+//     </p>
+//     <ul>
+//       @for(item of listaTarefas; track item; let idx = $index) {
+//         <li>{{ item }} <button (click)="remover(idx)"> - </button> </li>
+//       }
+//     </ul> 
+//   `,
+// })
+// export class Playground {
+//   tarefa = 'Arrumar o carro';
+//   listaTarefas: string[] = [];
+//   adicionar() {
+//     this.listaTarefas.push(this.tarefa);
+//   }
+//   remover(index: number) {
+//     this.listaTarefas.splice(index, 1);
+//   }
+// }
+// bootstrapApplication(Playground);
---
+++ EstruturaRepeticao/repeticaoFor.js
@@ -0,0 +1,5 @@
+var soma = 0;
+for (var par = 2; par <= 100; par += 2) {
+    soma += par;
+}
+console.log("Soma dos pares de 2 a 100 = " + soma);
