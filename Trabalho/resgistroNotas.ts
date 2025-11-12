246ef8c756f2b4d516cf3f3faaa40e51801c6b8e
Kaua-Henrique1 <kaua2006henrique023@gmail.com>
2025-11-12 11:48:48 -0300

    Correcoes dos metodos e adicao de get, set e construct

--- Trabalho/resgistroNotas.ts
+++ Trabalho/resgistroNotas.ts
@@ -8,31 +8,110 @@ export enum SituacaoEstudante {
 }
 
 export class registroNotas { 
-    private nomeEstudante: string;
-    private matriculaEstudante: number;
-    private situacaoEstudante: SituacaoEstudante;
-    private mediaPorBimestre1: number;
-    private mediaPorBimestre2: number;
-    private mediaParcial: undefined | number;
-    private mediaFinal: undefined | number;
-    private notaAvaliacaoFinal: undefined | number; 
+    private _nomeEstudante: string;
+    private _matriculaEstudante: number;
+    private _situacaoEstudante: SituacaoEstudante = SituacaoEstudante.CURSANDO;
+    private _mediaPorBimestre1: number;
+    private _mediaPorBimestre2: number;
+    private _mediaParcial: undefined | number = undefined;
+    private _mediaFinal: undefined | number = undefined;
+    private _notaAvaliacaoFinal: undefined | number; 
 
+    constructor(_nomeEstudante: string,
+            _matriculaEstudante: number,
+            _situacaoEstudante: SituacaoEstudante = SituacaoEstudante.CURSANDO,
+            _mediaPorBimestre1: number,
+            _mediaPorBimestre2: number,
+            _mediaParcial: undefined | number = undefined,
+            _mediaFinal: undefined | number = undefined,
+            _notaAvaliacaoFinal: undefined | number) {
+        this.nomeEstudante = _nomeEstudante
+        this.matriculaEstudante=_matriculaEstudante
+        this.situacaoEstudante=_situacaoEstudante
+        this.mediaPorBimestre1=_mediaPorBimestre1
+        this.mediaPorBimestre2=_mediaPorBimestre2
+        this.mediaParcial =_mediaParcial
+        this.mediaFinal = _mediaFinal
+        this.notaAvaliacaoFinal = _notaAvaliacaoFinal
+
+    }
+    
+    public get mediaPorBimestre1(): number {
+        return this._mediaPorBimestre1;
+    }
+    public set mediaPorBimestre1(value: number) {
+        this._mediaPorBimestre1 = value;
+    }
+    public get mediaPorBimestre2(): number {
+        return this._mediaPorBimestre2;
+    }
+    public set mediaPorBimestre2(value: number) {
+        this._mediaPorBimestre2 = value;
+    }
+    public get mediaParcial(): undefined | number {
+        return this._mediaParcial;
+    }
+    public set mediaParcial(value: undefined | number) {
+        this._mediaParcial = value;
+    }
+    public get mediaFinal(): undefined | number {
+        return this._mediaFinal;
+    }
+    public set mediaFinal(value: undefined | number) {
+        this._mediaFinal = value;
+    }
+    public get situacaoEstudante(): SituacaoEstudante {
+        return this._situacaoEstudante;
+    }
+    public set situacaoEstudante(value: SituacaoEstudante) {
+        this._situacaoEstudante = value;
+    }
+    public get nomeEstudante(): string {
+        return this._nomeEstudante;
+    }
+    public set nomeEstudante(value: string) {
+        this._nomeEstudante = value;
+    }
+    
+    public get matriculaEstudante(): number {
+        return this._matriculaEstudante;
+    }
+    public set matriculaEstudante(value: number) {
+        this._matriculaEstudante = value;
+    }
+    public get notaAvaliacaoFinal(): undefined | number {
+        return this._notaAvaliacaoFinal;
+    }
+    public set notaAvaliacaoFinal(value: undefined | number) {
+        this._notaAvaliacaoFinal = value;
+    }
+    
     public calcularMedia() {
-        this.mediaParcial = (this.mediaPorBimestre1 * 2 + this.mediaPorBimestre2 * 3) / 5
-        if (this.mediaParcial >= 60 ) {
-            this.situacaoEstudante = SituacaoEstudante.APROVADO
-            this.mediaFinal = this.mediaParcial
-        } else if (this.mediaParcial >= 10) {
-            this.situacaoEstudante = SituacaoEstudante.PROVAFINAL
+        this._mediaParcial = (this._mediaPorBimestre1 * 2 + this._mediaPorBimestre2 * 3) / 5
+        if (this._mediaParcial >= 60 ) {
+            this._situacaoEstudante = SituacaoEstudante.APROVADO
+            this._mediaFinal = this._mediaParcial
+        } else if (this._mediaParcial >= 10) {
+            this._situacaoEstudante = SituacaoEstudante.PROVAFINAL
         } else {
-            this.situacaoEstudante = SituacaoEstudante.REPROVADO
-            this.mediaFinal = this.mediaParcial
+            this._situacaoEstudante = SituacaoEstudante.REPROVADO
+            this._mediaFinal = this._mediaParcial
         }
     }
 
     public calcularAvaliacaoFinal(notaAvaliacaoFinal: number) {
-        this.notaAvaliacaoFinal = notaAvaliacaoFinal
-        this.mediaFinal = (notaAvaliacaoFinal + this.mediaParcial) / 2
-        return this.mediaFinal
+        this._notaAvaliacaoFinal = notaAvaliacaoFinal
+        
+        if (this._mediaParcial !== undefined) {
+            this._mediaFinal = (notaAvaliacaoFinal + this._mediaParcial) / 2;
+
+            if (this._mediaFinal >= 60) {
+                this._situacaoEstudante = SituacaoEstudante.APROVADO;
+            } else {
+                this._situacaoEstudante = SituacaoEstudante.REPROVADO;
+            }   
+            return this._mediaFinal;
+
+        }
     }
 }
